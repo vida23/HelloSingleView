6d847dbc2923586a83b1429312007090d0aa0c1d
Vida <vidageetirashidi@gmail.com>
2019-11-05 11:03:33 +0100

    Added button

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Vida Rashidi on 2019-11-05.
+//  Copyright © 2019 Vida Rashidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ButtonAct1(_ sender: Any) {
+        print("Hello Button")
+    }
+    
+}
